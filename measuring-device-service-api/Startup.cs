ed7000ff61ac081eb5c9c5b529cd244f23acc115
Csaba Gyuris <gyuris.csaba@vasvari.org>
2022-11-28 15:52:12 +0100

    El is indulnak

--- measuring-device-service-api/Startup.cs
+++ measuring-device-service-api/Startup.cs
@@ -47,7 +47,7 @@ namespace MeasureDeviceServiceAPIProject
             services.AddSingleton<IMeasureDevice30, MeasureDevice30>();
             services.AddSingleton<IHostedService, MeasureDevice10>(provider =>(MeasureDevice10) provider.GetService<IMeasureDevice10>());
             services.AddSingleton<IHostedService, MeasureDevice20>(provider =>(MeasureDevice20) provider.GetService<IMeasureDevice20>());
-            services.AddSingleton<IHostedService, MeasureDevice30>(provider => (MeasureDevice30)provider.GetService<IMeasureDevice20>());
+            services.AddSingleton<IHostedService, MeasureDevice30>(provider => (MeasureDevice30)provider.GetService<IMeasureDevice30>());
             //services.AddHostedService<MeasureDevice20>();
             //services.AddHostedService<MeasureDevice30>(provider => provider.GetService<MeasureDevice30>());
         }
